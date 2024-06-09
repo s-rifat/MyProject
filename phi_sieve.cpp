7b64d5c76b168ff25909f454cf71bff04fe9041f
Syed Rifat Manjur <syed.rifat.manjur@gmail.com>
2024-06-09 21:35:16 +0600

    add 4th method

--- phi_sieve.cpp
+++ phi_sieve.cpp
@@ -76,3 +76,26 @@ void phiSieve3(int n)
 
     //coment added
 }
+
+void phiSieve4(int n)
+
+{
+    for(int i=1; i<=n;i++)
+        phi[i] = i;
+
+    for(int i=2; i<=n; i++ )
+    {
+        if(phi[i]==i)
+        {
+            phi[i] = i-1;
+            for(int j= 2*i;j<=n;j+=i)
+            {
+                phi[j] = (phi[j]/i)*(i-1);
+            }
+        }
+    }
+
+    //coment added
+}
+
+
